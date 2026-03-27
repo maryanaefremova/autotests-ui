560652513331782d389f2d418b860ac828ca5d59
maryanaefremova <maryana.efremova@gmail.com>
2026-03-27 19:26:56 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -27,17 +27,29 @@ with sync_playwright() as playwright:
 
     # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
     context.storage_state(path="browser-state.json")
-    
-    # Проверяем наличие заголовка Dashboard
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-    expect(dashboard_title).to_have_text("Dashboard")
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    page.wait_for_timeout(5000)
+    # Проверяем наличие заголовка Courses
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text("Courses")
+
+    # Проверяем наличие There is no results
+    courses_list_empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(courses_list_empty_title).to_be_visible()
+    expect(courses_list_empty_title).to_have_text("There is no results")
+
+    # Проверяем наличие иконки пустого блока
+    courses_list_empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(courses_list_empty_icon).to_be_visible()
+
+    # Проверяем наличие Results from the load test pipeline will be displayed here
+    courses_list_empty_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(courses_list_empty_text).to_be_visible()
+    expect(courses_list_empty_text).to_have_text("Results from the load test pipeline will be displayed here")
